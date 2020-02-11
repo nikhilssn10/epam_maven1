860d3cdc1567f2af49df09ebe8b22d10da04749f
Nikhil Satya <nikhilssn10@gmail.com>
2020-02-11 10:43:44 +0530

    initial commit

---
+++ task2/src/main/java/epam/nikhil_maven/cholocates/Barone.java
@@ -0,0 +1,8 @@
+package epam.nikhil_maven.cholocates;
+
+public class Barone extends Chocolate {
+
+    public Barone(String name, int price, int weight) {
+        super(name, price, weight);
+    }
+}
---
+++ task2/src/main/java/epam/nikhil_maven/cholocates/Galaxy.java
@@ -0,0 +1,8 @@
+package epam.nikhil_maven.cholocates;
+
+public class Galaxy extends Chocolate {
+
+    public Galaxy(String name,int price,int weight){
+        super(name,price,weight);
+    }
+}
